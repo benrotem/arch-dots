13c2592206bbec512951a415ea6cdaac5892b3a8
Ben <brotem321@gmail.com>
2026-06-10 18:44:35 +1000

    Apply rounded corners to windows with picom

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -538,3 +538,8 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- {{{ Autostart
+-- Apply rounded corners to windows (requires picom)
+awful.spawn.with_shell("picom --backend glx --corner-radius 18")
+-- }}}
