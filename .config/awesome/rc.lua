894dc61eead253c2af6edf32d4e2a39c99c6b643
Ben <brotem321@gmail.com>
2026-06-10 15:55:22 +1000

    Remove tasklist and layoutbox widgets from wibar

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -207,13 +207,12 @@ awful.screen.connect_for_each_screen(function(s)
             s.mytaglist,
             s.mypromptbox,
         },
-        s.mytasklist, -- Middle widget
+        -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
