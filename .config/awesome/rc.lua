11587a056a5d447fd8142d35c88665e8f50ce003
Ben <brotem321@gmail.com>
2026-06-10 14:03:30 +1000

    Set keybind to close window

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -342,8 +342,8 @@ clientkeys = gears.table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
-              {description = "close", group = "client"}),
+    awful.key({ modkey,           }, "c",      function (c) c:kill()                         end,
+              {description = "close window", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
