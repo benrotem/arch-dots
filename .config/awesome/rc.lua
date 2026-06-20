1f7400fc7ac689e23bf0c5a15f8befebbc8b803b
Ben <brotem321@gmail.com>
2026-06-20 14:59:06 +0300

    Remove systray widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -238,7 +238,6 @@ awful.screen.connect_for_each_screen(function(s)
 	},
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            wibox.widget.systray(),
             batwidget_container,
         },
     }
