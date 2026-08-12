a7a5aba681ab047110293de112901d1aa281d305
Ben <brotem321@gmail.com>
2026-08-13 07:48:16 +1000

    Install tree-sitter-manager plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,10 @@
-vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+vim.pack.add {
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin", },
+	{ src = "https://github.com/romus204/tree-sitter-manager.nvim", },
+}
+
+require("tree-sitter-manager").setup()
+
 vim.cmd.colorscheme "catppuccin"
 
 vim.wo.number = true
