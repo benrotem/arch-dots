bf2c4ceafda74f74314be7b00c83a67aae5d578f
Ben <brotem321@gmail.com>
2026-08-18 17:51:46 +1000

    Enable clangd linting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,11 @@
 vim.pack.add {
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin", },
 	{ src = "https://github.com/romus204/tree-sitter-manager.nvim", },
+	{ src = "https://github.com/neovim/nvim-lspconfig", },
 }
 
 require("tree-sitter-manager").setup()
+vim.lsp.enable('clangd')
 
 vim.cmd.colorscheme "catppuccin"
 
