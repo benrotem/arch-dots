b31f75f23a22e4ed97ab592a1606ae2cca6ad2b7
Ben <brotem321@gmail.com>
2026-08-27 15:43:11 +1000

    Set tab as two spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,3 +11,9 @@ vim.cmd.colorscheme "catppuccin"
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Set tab as two spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
