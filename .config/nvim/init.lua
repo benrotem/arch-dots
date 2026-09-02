0844797850279a6189664e2acab5d78006f64278
Ben <brotem321@gmail.com>
2026-09-02 11:34:36 +1000

    Restructure and section code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,25 @@
+-- Plugins
 vim.pack.add {
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin", },
 	{ src = "https://github.com/romus204/tree-sitter-manager.nvim", },
 	{ src = "https://github.com/neovim/nvim-lspconfig", },
 }
 
+-- Colours
 require("tree-sitter-manager").setup()
+vim.cmd.colorscheme "catppuccin"
+
+-- Set hybrid line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Set tab as two spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
+-- Language servers
 vim.lsp.enable('clangd')
 
 vim.lsp.config('texlab', {
@@ -21,14 +36,3 @@ vim.lsp.config('texlab', {
   },
 })
 vim.lsp.enable('texlab')
-
-vim.cmd.colorscheme "catppuccin"
-
-vim.wo.number = true
-vim.wo.relativenumber = true
-
--- Set tab as two spaces
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
